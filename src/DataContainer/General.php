d1b9a540e73930c888ba83b4108976b6fd7ad851
Stefan Gruna <stefan@designbuero-gruna.de>
2019-06-01 12:27:57 +0200

    Show custom template names instead of file names for articles, elements, module, forms and form fields.

---
+++ src/DataContainer/General.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Slashworks\ContaoStarterBundle\DataContainer;
+
+use Contao\Backend;
+use Contao\DataContainer;
+
+class General extends Backend
+{
+
+    /**
+     * Instead of showing the file name in the backend, show a custom translation.
+     *
+     * @param DataContainer $dc
+     *
+     * @return array
+     */
+    public function getCustomTemplateNames(DataContainer $dc)
+    {
+        $namedTemplates = array();
+        $templates = array();
+        
+        $table = $dc->table;
+        $type = $dc->activeRecord->type;
+
+        switch ($table) {
+            case 'tl_article':
+                $templates = $this->getTemplateGroup('mod_article');
+                $defaultTemplateKey = 'mod_article';
+                break;
+            case 'tl_content':
+                $templates = $this->getTemplateGroup('ce_' . $type);
+                $defaultTemplateKey = 'ce_' . $type;
+                break;
+            case 'tl_form':
+                $templates = $this->getTemplateGroup('form_wrapper_');
+                $defaultTemplateKey = 'form_wrapper';
+                break;
+            case 'tl_form_field':
+                $templates = $this->getTemplateGroup('form_' . $type);
+                $defaultTemplateKey = 'form_' . $type;
+                break;
+            case 'tl_module':
+                $templates = $this->getTemplateGroup('mod_' . $type);
+                $defaultTemplateKey = 'mod_' . $type;
+                break;
+            default:
+                break;
+        }
+
+        foreach ($templates as $key => $value) {
+            if ($key === $defaultTemplateKey) {
+                $namedTemplates[$key] = $GLOBALS['TL_LANG'][$table]['customTemplateNames']['default'];
+                continue;
+            }
+
+            if (isset($GLOBALS['TL_LANG'][$table]['customTemplateNames'][$key])) {
+                $namedTemplates[$key] = $GLOBALS['TL_LANG'][$table]['customTemplateNames'][$key];
+            } else {
+                $namedTemplates[$key] = $value;
+            }
+        }
+        
+        return $namedTemplates;
+    }
+    
+}
---
+++ src/Resources/contao/languages/de/tl_form_field.php
@@ -0,0 +1,6 @@
+<?php
+
+/**
+ * Custom template names
+ */
+$GLOBALS['TL_LANG']['tl_form_field']['customTemplateNames']['default'] = 'Standard';
