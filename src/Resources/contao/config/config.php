07d7477826bbae5b34ef68689b2553f9897557ad
Stefan Gruna <stefang@slash-works.de>
2019-06-04 11:50:23 +0200

    Remove double declaration of news backend module.

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -11,16 +11,6 @@ $GLOBALS['BE_MOD']['system']['contaostarter'] = array
     'callback' => ContaoStarterInstall::class,
 );
 
-array_insert($GLOBALS['BE_MOD']['content'], 1, array
-(
-    'news' => array
-    (
-        'tables'      => array('tl_news_archive', 'tl_news', 'tl_news_feed', 'tl_content'),
-        'table'       => array('TableWizard', 'importTable'),
-        'list'        => array('ListWizard', 'importList')
-    )
-));
-
 
 /**
  * Hooks
